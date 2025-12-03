684645eb8bafc4e9e0e21f8aafc2da05c8efceb4
Joker-V2 <nipinna0@gmail.com>
2025-12-03 16:09:35 +0100

    Revert "Allow automated sideload from within recovery."
    This reverts commit 5726f420a484b068f290031301b047a17ca1c02d.
    
    Change-Id: I0a258a2804ec8128094e8b6e3f2355511ef96076

--- recovery_main.cpp
+++ recovery_main.cpp
@@ -240,7 +240,6 @@ static void ListenRecoverySocket(RecoveryUI* ui, std::atomic<Device::BuiltinActi
     char msg;
     constexpr char kSwitchToFastboot = 'f';
     constexpr char kSwitchToRecovery = 'r';
-    constexpr char kSwitchToSideload = 's';
     ssize_t ret = TEMP_FAILURE_RETRY(read(connection_fd, &msg, sizeof(msg)));
     if (ret != sizeof(msg)) {
       PLOG(ERROR) << "Couldn't read from socket";
@@ -252,9 +251,6 @@ static void ListenRecoverySocket(RecoveryUI* ui, std::atomic<Device::BuiltinActi
         break;
       case kSwitchToFastboot:
         action = Device::BuiltinAction::ENTER_FASTBOOT;
-        break;
-      case kSwitchToSideload:
-        action = Device::BuiltinAction::APPLY_ADB_SIDELOAD;
         break;
       default:
         LOG(ERROR) << "Unrecognized char from socket " << msg;
@@ -524,8 +520,6 @@ int main(int argc, char** argv) {
   std::thread listener_thread(ListenRecoverySocket, ui, std::ref(action));
   listener_thread.detach();
 
-  Device::BuiltinAction next_recovery_action = Device::NO_ACTION;
-
   // Set up adb_keys and enable root before starting ADB.
   if (IsRoDebuggable() && !fastboot) {
     copy_userdata_files();
@@ -553,21 +547,7 @@ int main(int argc, char** argv) {
       }
     }
 
-    Device::BuiltinAction ret;
-    if (fastboot) {
-      ret = StartFastboot(device, args);
-    } else {
-      // If we received a sideload command via the recovery socket, then
-      // force recovery into sideload mode.
-      if (next_recovery_action == Device::APPLY_ADB_SIDELOAD) {
-        ret = start_recovery(device, { args[0], "--sideload" });
-      } else {
-        ret = start_recovery(device, args);
-      }
-    }
-
-    // Reset the next recovery action.
-    next_recovery_action = Device::NO_ACTION;
+    auto ret = fastboot ? StartFastboot(device, args) : start_recovery(device, args);
 
     if (ret == Device::KEY_INTERRUPTED) {
       ret = action.exchange(ret);
@@ -642,10 +622,6 @@ int main(int argc, char** argv) {
       case Device::REBOOT_FROM_FASTBOOT:
         ui->Print("Rebooting...\n");
         Reboot("userrequested,fastboot");
-        break;
-
-      case Device::APPLY_ADB_SIDELOAD:
-        next_recovery_action = ret;
         break;
 
       default:
